90ff33bd4ee603501b7b5bf558a8e5497f888a75
BlaiseCz <blazej.czekala@gmail.com>
2020-01-27 15:27:36 +0100

    27.01 commit

--- gote_external_urls.php
+++ gote_external_urls.php
@@ -4,29 +4,35 @@
     use TheCodingMachine\Gotenberg\Client;
     use TheCodingMachine\Gotenberg\DocumentFactory;
     use TheCodingMachine\Gotenberg\OfficeRequest;
+    use TheCodingMachine\Gotenberg\Request;
 
     // $url = 'https://bip.poznan.pl/bip/attachments.att?co=show&instance=1057&parent=37297&lang=pl&id=309423';
-    $url = 'http://poznan.pl/public/bip/attachments.att?co=show&instance=1058&parent=37297&lang=pl&id=312378';
-    $file_name = 'test1.pdf';
+    // $url = 'http://poznan.pl/public/bip/attachments.att?co=show&instance=1058&parent=37297&lang=pl&id=312378';
+    // $url = 'http://poznan.pl/public/bip/attachments.att?co=show&instance=1057&parent=37297&lang=pl&id=309422';
+    $url = 'http://poznan.pl/public/bip/attachments.att?co=show&instance=1057&parent=37297&lang=pl&id=309410';
+    $file_name = 'test4.pdf';
+    $client_path = 'http://localhost:3000';
 
-    create_pdf($url, $file_name);
+    create_pdf($url, $file_name, $client_path);
 
-    function create_pdf($url, $file_name) {
+    function create_pdf($url, $file_name, $client_path) {
 
         $file_string = get_file_string_from_url($url);
         $pathTofiles = '/var/www/html/testpdfjs/pdfs';
 
-        $client = new Client('http://localhost:3000', new \Http\Adapter\Guzzle6\Client());
+        $client = new Client($client_path, new \Http\Adapter\Guzzle6\Client());
     
         $files = [
             DocumentFactory::makeFromString('test.doc',  $file_string),
         ];
 
-        try {
-                    
+        try {        
+            echo "processing...\n";        
             $request = new OfficeRequest($files);
             $request->setWaitTimeout(20);
-            $request->setLandscape(true);
+            // $request->setPaperSize(Request::A4);
+            // $request->setMargins(Request::NO_MARGINS);
+            // $request->setLandscape(true);
 
             $client->store($request, $pathTofiles.'/'.$file_name);
             $client->post($request);  
@@ -40,28 +46,14 @@
     }
 
     function get_file_string_from_url($url) {
-           
-        //Download the file using file_get_contents.
+        
         $file_string = file_get_contents($url);
         
-        // var_dump($downloadedFileContents);
-        //Check to see if file_get_contents failed.
         if($file_string === false){
             throw new Exception('Failed to download file at: ' . $url);
         }
         
-        //The path and filename that you want to save the file to.
-        $fileName = 'downloaded.doc';
-        
-        //Save the data using file_put_contents.
-        $save = file_put_contents($fileName, $file_string);
-        
-        //Check to see if it failed to save or not.
-        if($save === false){
-            throw new Exception('Failed to save file to: ' , $fileName);
-        }
-
-        echo "bytes retrieved...\n";
+        echo "content retrieved...\n";
         return $file_string;
     }
 ?>
---
+++ pdfviewer.js
@@ -0,0 +1,125 @@
+window.onload = function() {
+
+    //read file src
+    document.getElementById('file-src').addEventListener('keypress', (e) => {
+        e = e || window.event;
+    
+    
+        if(e.keyCode == 13) {
+            var elem = e.srcElement || e.target;
+    
+    
+            if (elem.value == null || elem.value == "") {
+                alert('empty file')
+            }
+            else {
+                if(isProperFileExtension(elem.value)) {
+                    getPDFsource(elem.value)
+                }
+            }
+        }
+    });
+   
+    function isProperFileExtension(fileSource) {
+        var last3 = fileSource.slice(fileSource.length -3, fileSource.length);
+        var last4 = fileSource.slice(fileSource.length -4, fileSource.length);
+        
+        alert(last3 + ' ' + last4)
+    
+        if(last3 == 'pdf' || last3 == 'doc' || last4 == 'docx') {
+            return true;
+        }
+    
+        return false;
+    
+    }
+    // PDF.JS viewing tool
+    var myState = {
+                pdf:null,
+                currentPage:1,
+                zoom:1
+            }
+    
+    function getPDFsource(pdfSource) {
+        pdfjsLib.getDocument(pdfSource).then(pdf => {
+                alert(pdfSource)
+                myState.pdf = pdf
+                render()
+            })
+    }
+    
+    function render() {
+        myState.pdf.getPage(myState.currentPage).then(page => {
+            var canvas = document.getElementById("pdf_renderer")
+            var ctx = canvas.getContext("2d")
+            var viewport = page.getViewport(myState.zoom, 0)
+    
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+    
+            //render
+            page.render({
+                canvasContext:ctx,
+                viewport:viewport
+            })
+        })
+    }
+    
+    /**
+     * PDF CONTROLLERS
+     * 
+     */
+    document.getElementById('go_previous').addEventListener('click', (e) => {
+        if(myState.pdf == null || myState.currentPage == 1) return; //pierwsza strona
+    
+        myState.currentPage = myState.currentPage - 1;
+        document.getElementById('current_page').value = myState.currentPage;
+    
+        render()
+    })
+    
+    document.getElementById('go_next').addEventListener('click', (e) => {
+        if(myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages) return; //ostatnia strona
+    
+        myState.currentPage = myState.currentPage + 1;
+        document.getElementById('current_page').value = myState.currentPage;
+    
+        render()
+    })
+    
+    document.getElementById('current_page').addEventListener('keypress', (e) => {
+        if(myState.pdf == null) return;
+    
+        //keycode
+        var code = (e.keyCode ? e.keyCode : e.which)
+    
+        if(code == 13)
+        {
+            var desiredPage = document.getElementById('current_page').valueAsNumber
+    
+            if(desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages)
+            {
+                myState.currentPage = desiredPage
+                document.getElementById('current_page').value = desiredPage;
+    
+                render();
+            }
+        }
+    })
+    
+    document.getElementById('zoom_in').addEventListener('click', (e) => {
+        if(myState.pdf == null) return;
+        if(myState.zoom < 3) {
+            myState.zoom = myState.zoom + 0.5
+            render()
+        }
+    })
+    
+    document.getElementById('zoom_out').addEventListener('click', (e) => {
+        if(myState.pdf == null) return;
+        if(myState.zoom > 0.5) {
+            myState.zoom = myState.zoom - 0.5
+            render()
+        }
+    })
+}
